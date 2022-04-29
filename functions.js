d01018fcd55e5419710b35046dcaa1257bf5f807
baileyfcross <bf02cros@siena.edu>
2022-04-28 22:47:37 -0400

    Added hard coded api call

--- functions.js
+++ functions.js
@@ -1,1 +1,20 @@
-function 
+function getInfo(){
+  var data = {};
+  $(document).ready(() => {
+    var name = document.getElementById("#countryName");
+    //`https://restcountries.com/v3.1/name/${name}`
+    $.ajax({
+       url : 'https://restcountries.com/v3.1/name/USA',
+       method : 'GET',
+       data: data,
+       success : function(result){
+         console.log(data);
+       },
+       error : function(result, statut, error){
+         console.log(error);
+         console.log(statut);
+         console.log(result);
+       }
+    });
+  });
+}
