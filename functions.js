a6468f0afd47b6ddd8eb38f8cbe6ee47315bfc31
baileyfcross <bf02cros@siena.edu>
2022-05-09 04:44:50 -0400

    added better rows and columns

--- functions.js
+++ functions.js
@@ -15,7 +15,7 @@ function getInfo(){
        data: data,
        success : function(result){
          var newData = JSON.stringify(result,null,4);
-         console.log(newData);
+         //console.log(newData);
          $("#errinfo").text("");
          clearData();
          filterData(result);
@@ -43,17 +43,17 @@ function filterData(result){
   /*
   Common name and official name for the country
   */
-  var commonName = $(`<div id="commonName" class ="info"><p>Common Name: ${result[0].name.common}</p></div>`)
-  var officialName = $(`<div id="officialName" class ="info"><p>Official Name: ${result[0].name.official}</p></div>`);
+  var commonName = $(`<div id="commonName" class ="info col-6"><p>Common Name: ${result[0].name.common}</p></div>`)
+  var officialName = $(`<div id="officialName" class ="info col-6"><p>Official Name: ${result[0].name.official}</p></div>`);
   $('#names').append(commonName);
   $('#names').append(officialName);
 
   /*
   Capital and Population information about the country
   */
-  var capital = $(`<div id="capital" class ="info"><p>Capital: ${result[0].capital[0]}</p></div>`);
-  var capitalCoords = $(`<div id="capitalCoords" class ="info"><p>Capital Coordinates: ${result[0].capitalInfo.latlng[0]}, ${result[0].capitalInfo.latlng[1]}</p></div>`);
-  var pop = $(`<div id="pop" class ="info"><p>Population: ${new Intl.NumberFormat().format(result[0].population)}</p></div>`)
+  var capital = $(`<div id="capital" class ="info col-3"><p>Capital: ${result[0].capital[0]}</p></div>`);
+  var capitalCoords = $(`<div id="capitalCoords" class ="info col-4"><p>Capital Coordinates: ${result[0].capitalInfo.latlng[0]}, ${result[0].capitalInfo.latlng[1]}</p></div>`);
+  var pop = $(`<div id="pop" class ="info col-3"><p>Population: ${new Intl.NumberFormat().format(result[0].population)}</p></div>`)
   $('#popAndCap').append(capital);
   $('#popAndCap').append(capitalCoords);
   $('#popAndCap').append(pop);
@@ -62,37 +62,40 @@ function filterData(result){
   Misc Infomation about the country
   */
   if(result[0].fifa != null){
-    var fifaInfo = $(`<div id="fifaInfo" class ="info"><p>Fifa Name: ${result[0].fifa}</p></div>`);
+    var fifaInfo = $(`<div id="fifaInfo" class ="info col-12"><p>Fifa Name: ${result[0].fifa}</p></div>`);
     $('#miscInfo').append(fifaInfo);
   }
   if(result[0].translations.deu != null){
-    var germInfo = $(`<div id="germInfo" class ="info"><p>Common Name in German: ${result[0].translations.deu.common}</p></div>`);
+    var germInfo = $(`<div id="germInfo" class ="info col-12"><p>Common Name in German: ${result[0].translations.deu.common}</p></div>`);
     $('#miscInfo').append(germInfo);
   }
   if(result[0].translations.zho != null){
-    var zhoInfo = $(`<div id="zhoInfo" class ="info"><p>Common Name in Chinese: ${result[0].translations.zho.common}</p></div>`);
+    var zhoInfo = $(`<div id="zhoInfo" class ="info col-12"><p>Common Name in Chinese: ${result[0].translations.zho.common}</p></div>`);
     $('#miscInfo').append(zhoInfo);
   }
   if(result[0].translations.ara != null){
-    var araInfo = $(`<div id="zhoInfo" class ="info"><p>Common Name in Arabic: ${result[0].translations.ara.common}</p></div>`);
+    var araInfo = $(`<div id="zhoInfo" class ="info col-12"><p>Common Name in Arabic: ${result[0].translations.ara.common}</p></div>`);
     $('#miscInfo').append(araInfo);
   }
-  var startInfo = $(`<div id="startInfo" class ="info"><p>Start of the Week: ${capitalizeFirstLetter(result[0].startOfWeek)}</p></div>`);
+  var startInfo = $(`<div id="startInfo" class ="info col-12"><p>Start of the Week: ${capitalizeFirstLetter(result[0].startOfWeek)}</p></div>`);
   $('#miscInfo').append(startInfo);
 
   /*
   Map Link, Coat of Arms and Flag of the country
   */
   if(result[0].maps != null){
-    var map = $(`<a href = "${result[0].maps.googleMaps}" target="_blank" rel="noreferrer noopener" id = "mapInfo" class = "info">Link to a Map of the Country</a>`)
+    var map = $(`<div class ="info col-12"><a href = "${result[0].maps.googleMaps}" target="_blank" rel="noreferrer noopener" id = "mapInfo" class = "info">Link to a Map of the Country</a></div>`)
     $("#mapItems").append(map);
   }
   if(result[0].flags != null){
-    var flag = $(`<p class ='info'>Country Flag: </p><img src ="${result[0].flags.png}" alt="countries flag" id="countryFlag" class="info">`);
+    var flag = $(`<div class ="info col-6"><p class ='info'>Country Flag: </p><img src ="${result[0].flags.png}" alt="countries flag" id="countryFlag" class="info"></div>`);
     $("#mapItems").append(flag);
   }
-  if(result[0].coatOfArms != null){
-    var coa = $(`<p class ='info'>Coat of Arms: </p><img src ="${result[0].coatOfArms.png}" alt="coat of arms" id="COA" class="info">`);
+  /*
+  If the Coat of Arms image is there, then add it to the page
+  */
+  if(result[0].coatOfArms.length != undefined || result[0].coatOfArms.png){
+    var coa = $(`<div class ='col-6'><p class ='info'>Coat of Arms: </p><img src ="${result[0].coatOfArms.png}" alt="coat of arms" id="COA" class="info"></div>`);
     $("#mapItems").append(coa);
   }
 }
