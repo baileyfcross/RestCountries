25e0fba2e40020fcb0b3bbaca465355343ecf93f
baileyfcross <bf02cros@siena.edu>
2022-05-09 05:11:57 -0400

    added comments to the js file

--- functions.js
+++ functions.js
@@ -1,21 +1,49 @@
+/*
+Restful Countries API project
+
+Created by: Bailey Cross
+Date: Spring 2022
+
+*/
+
+/*
+This code makes it so that the user can't press
+the enter key to submit on the input
+*/
 $(document).on("keydown", "form", function(event) {
     return event.key != "Enter";
 });
 
+/*
+This is the main function of the program
+It gathers information from an ajax get request
+*/
 function getInfo(){
+  /*
+  data is just here so that js doesn't yell at me
+  */
   var data = {};
   $(document).ready(() => {
+    //Gets the countries name from the form
     var name = $("#countryName").val();
-    console.log(name);
-    //`https://restcountries.com/v3.1/name/${name}`
-    //'https://restcountries.com/v3.1/name/USA'
+    /*
+    Test APIs
+    `https://restcountries.com/v3.1/name/${name}`
+    'https://restcountries.com/v3.1/name/USA'
+    */
     $.ajax({
        url : `https://restcountries.com/v3.1/name/${name}`,
        method : 'GET',
        data: data,
        success : function(result){
+         /*
+         puts all of the returned data into the console neatly
+         */
+         /*
          var newData = JSON.stringify(result,null,4);
-         //console.log(newData);
+         console.log(newData);
+         */
+
          $("#errinfo").text("");
          clearData();
          filterData(result);
@@ -28,20 +56,32 @@ function getInfo(){
   });
 }
 
+/*
+Capitalizes the first letter of the data
+*/
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-
+/*
+Clears all of the old data in the form so that the new data can come in
+*/
 function clearData(){
   $("#countryFlag").remove();
   $("#COA").remove();
   $('#mapInfo').remove();
   $(".info").remove();
 }
+
+/*
+This is the helper function of the ajax call
+It is so that everything can be broken down and easily read
+*/
 function filterData(result){
   /*
   Common name and official name for the country
+  Common Country Name
+  Official Country Name
   */
   var commonName = $(`<div id="commonName" class ="info col-6"><p>Common Name: ${result[0].name.common}</p></div>`)
   var officialName = $(`<div id="officialName" class ="info col-6"><p>Official Name: ${result[0].name.official}</p></div>`);
@@ -50,6 +90,9 @@ function filterData(result){
 
   /*
   Capital and Population information about the country
+  Capital
+  Capital Coordinates
+  Population
   */
   var capital = $(`<div id="capital" class ="info col-3"><p>Capital: ${result[0].capital[0]}</p></div>`);
   var capitalCoords = $(`<div id="capitalCoords" class ="info col-4"><p>Capital Coordinates: ${result[0].capitalInfo.latlng[0]}, ${result[0].capitalInfo.latlng[1]}</p></div>`);
@@ -60,6 +103,10 @@ function filterData(result){
 
   /*
   Misc Infomation about the country
+  Fifa Name
+  German Translation
+  Chinese Translation
+  Arabic Translation
   */
   if(result[0].fifa != null){
     var fifaInfo = $(`<div id="fifaInfo" class ="info col-12"><p>Fifa Name: ${result[0].fifa}</p></div>`);
@@ -82,6 +129,9 @@ function filterData(result){
 
   /*
   Map Link, Coat of Arms and Flag of the country
+  Map Link
+  Flag
+  Coat of Arms
   */
   if(result[0].maps != null){
     var map = $(`<div class ="info col-12"><a href = "${result[0].maps.googleMaps}" target="_blank" rel="noreferrer noopener" id = "mapInfo" class = "info">Link to a Map of the Country</a></div>`)
